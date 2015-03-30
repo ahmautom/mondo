31134883bad0dbcb011bc11649296edf1903bdd0
Mau Ka Ming <ahmautom@gmail.com>
2015-03-31 01:22:30 +0800

    promisify insert

--- src/stores/abstractwebstorage.js
+++ src/stores/abstractwebstorage.js
@@ -34,7 +34,7 @@ AbstractWebStorageStore.prototype._save = function(collectionName, array) {
 // Read
 
 AbstractWebStorageStore.prototype.filter = function(collection, query, next) {
-    var docs = this._get(query.collectionName); 
+    var docs = this._get(query.collectionName);
     return Q.Promise(function(resolve, reject, notify) {
         if (options.transform && !query.options.lean) {
             resolve(utils.map(docs, options.transform));
@@ -46,24 +46,16 @@ AbstractWebStorageStore.prototype.filter = function(collection, query, next) {
 
 // Write
 
-AbstractWebStorageStore.prototype.insert = function(collection, query, deferred, next) {
+AbstractWebStorageStore.prototype.insert = function(collection, query, next) {
     var array = this._get(query.collectionName);
-    var docs;
-
-    if (utils.isArray(query.doc)) {
-        docs = query.doc;
-    } else {
-        docs = [query.doc];
-    }
-
-    array = array.concat(docs);
-    this._save(query.collectionName, array);
-    var models = utils.map(docs, function(doc) {
-        return new Model(doc);
+    var promise = Q.Promise(function(resolve, reject, notify) {
+        var docs = utils.isArray(query.doc) ? query.doc : [query.doc];
+        array = array.concat(docs);
+        this._save(query.collectionName, array);
+        var models = utils.map(docs, function(doc) {
+            return new Model(doc);
+        });
+        resolve(models);
     });
-    setTimeout(function() {
-        deferred.resolve(models);
-    }, 0);
-
-    return next();
+    return Q.all([promise, next()]);
 };
